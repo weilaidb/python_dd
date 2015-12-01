def95a8581ace4f413f9f1937beaaf70cfe5a267
Your Name <you@example.com>
2015-12-01 04:37:11 -0800

    backup files python script

--- 27/backup_ver2.py
+++ 27/backup_ver2.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
-#Filename:backup_ver1.py
+#Filename:backup_ver2.py
 import os
 import time
 #1.The files and directories to be backed up are specified in a list.
 source = ['/home/weilaidb/Music/','/home/weilaidb/test']
 #If you are using Windows,use source = [r'C:\Documents',r'D:\Work'] or something like that
 #2.The backup must be stored in a main backup directory
-#target_dir = '/mnt/backup/'
-target_dir = 'backup/'
-#3.The files are backed up into a zip file
-#4.The name of the zip archive is the current date and time
-target = target_dir + time.strftime('%Y%m%d%H%M%S')+'.zip'
+target_dir = 'bk/' #Remeber to change this to what you will be using
+#3.The files are backed up into a zip file.
+#4.The current day is the name of the subdirectory in the main directory
+today = target_dir + time.strftime('%Y%m%d')
+#The current time is the name of the zip archive
+now =  time.strftime('%H%M%S')
+#Create the subdirectory if it isn't already there
+if not os.path.exists(today):
+	os.mkdir(today) #make directory
+	print 'Successfully created directory',today
+
+#The name of the zip file
+target = today + os.sep + now + '.zip'
 #5.We use the zip command(in Unix/Linux) to put the files in a zip archive
 zip_command = "zip -qr '%s' %s" % (target, ' '.join(source))
 #Run the backup
@@ -18,4 +26,3 @@ if os.system(zip_command) == 0:
 	print 'Successful backup to',target
 else:
 	print 'Backup FAILED'
-
